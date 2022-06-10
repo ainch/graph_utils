1701eeab9c5a68ea22df7db9a3c92e32ffd3ac69
ainch <chldbsdu1001@naver.com>
2022-06-10 21:02:45 +0900

    add transformation function

--- test.py
+++ test.py
@@ -14,6 +14,14 @@ def test_graph_format():
     print(G.nodes.data())
     print(G.edges())
 
+def test_gfu_format_to_graph_format():
+    path_gfu = 'example/g.gfu'
+    path_out_graph = 'example/g_out.graph'
+    
+    G = graph.get_networkx_gfu_format(path_gfu)
+    graph.make_file_graph_format(G, path_out_graph)
+
 if __name__:
     test_gfu_format()
     test_graph_format()
+    test_gfu_format_to_graph_format()
