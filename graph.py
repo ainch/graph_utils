66109768d36abdf9ce2a9ef9420f5e1bcff2ed34
ainch <chldbsdu1001@naver.com>
2022-06-10 20:40:29 +0900

    add graph format test

--- graph.py
+++ graph.py
@@ -1,9 +1,9 @@
 import networkx as nx
 
-def get_networkx_gfu(name_path):
+def get_networkx_gfu_format(name_path):
 	G = nx.Graph()
 	
-	with open(name_path) as f:
+	with open(name_path,'r') as f:
 		_ = f.readline()
 		
 		num_vertices = int(f.readline())
@@ -17,5 +17,24 @@ def get_networkx_gfu(name_path):
 		for idx in range(num_edges):
 			x,y = map(int, f.readline().split())
 			G.add_edge(x, y)
+
+	return G
+
+def get_networkx_graph_format(name_path):
+	G = nx.Graph()
+	
+	with open(name_path, 'r') as f:
+		_, x, y = f.readline().split()
+		
+		num_vertices = int(x)
+		num_edges = int(y)
+		
+		for _ in range(num_vertices):
+			_, v, l, d = f.readline().split()
+			G.add_node(int(v), label = l)
+		
+		for _ in range(num_edges):
+			_, x, y = f.readline().split()
+			G.add_edge(int(x), int(y))
 
 	return G
--- test.py
+++ test.py
@@ -1,11 +1,18 @@
 import graph
 
-def test_gfu():
+def test_gfu_format():
     path_gfu = 'example/g.gfu'
-    G = graph.get_networkx_gfu(path_gfu)
+    G = graph.get_networkx_gfu_format(path_gfu)
+    print(G.nodes())
+    print(G.nodes.data())
+    print(G.edges())
+
+def test_gfu_graph_format():
+    path_graph = 'example/g.graph'
+    G = graph.get_networkx_graph_format(path_graph)
     print(G.nodes())
     print(G.nodes.data())
     print(G.edges())
 
 if __name__:
-    test_gfu()
+    test_gfu_graph_format()
