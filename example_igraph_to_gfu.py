1e2f95a78ebafa8076733f8a9b6cd383e6ee5392
yunyoung Choi <yunyoungchoi@yunyoungs-MacBook-Air.local>
2022-09-26 10:11:43 +0900

    make example with mulitple graphs in an igraph file

--- example_igraph_to_gfu.py
+++ example_igraph_to_gfu.py
@@ -1,9 +1,12 @@
 from itertools import product
 import graph
+import os
 
 def simple():
-    G = graph.get_networkx_igraph_format('example/g2.igraph')
-    graph.make_file_gfu_format(G, 'example/g2.gfu')
+    Gs = graph.get_networkx_igraph_format('example/g2.igraph')
+    print(Gs)
+    graph.make_file_gfu_format(Gs[0], 'example/g2_a.gfu')
+    graph.make_file_gfu_format(Gs[1], 'example/g2_b.gfu')
 
 if __name__=='__main__':
     simple()
